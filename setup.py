c087bdcec33298f7b8bea59fc38398a4580c03ee
Greg Hayes <hayesgb@gmail.com>
2018-04-15 16:58:24 -0500

    Added azure example files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comparison_shopper_site',
+    version='0.0.1',
+    description='Website to compare prices of household goods',
+    url='https://compshopper.azurewebsites.net',
+    license='Apache 2.0',
+    packages=find_packages()
+)
